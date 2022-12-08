e0afa00f5d16eedd92bc362abcfb7c885aabfe05
Sunlight-9884 <117644205+Sunlight-9884@users.noreply.github.com>
2022-12-08 07:12:45 +0630

    Multichoice

---
+++ Question.java
@@ -0,0 +1,10 @@
+package multiplechoicequiz;
+public class Question{
+	String prompt;
+	String answer;
+	Question(String prompt,String answer){
+		this.prompt=prompt;
+		this.answer=answer;
+	}
+}
+
